ef2c1434fc63baa9af2e4e9c4b3b270577e0ccd6
Helge Mathee <helge.mathee@fabricengine.com>
2017-03-21 10:30:24 +0100

    added zlib build, patching hdf5, switching to VS2015

--- build-usd-windows.py
+++ build-usd-windows.py
@@ -14,16 +14,16 @@ if target == __file__:
 allowedTargets = [
   'clean',
   'all',
+  'zlib', 
   'tbb', 
   'double-conversion',
   'ilmbase',
   'hdf5',
   'openexr',
   'ptex',
-  'glew',
-  'glut',
   'opensubdiv',
   'alembic',
+  'usd',
 ]
 if not target in allowedTargets:
   print """
@@ -38,15 +38,11 @@ target: %s
 root = os.path.abspath(os.path.split(__file__)[0])
 build = os.path.join(root, 'build')
 stage = os.path.join(root, 'stage')
-vsversion = '12'
+vsversion = '14'
 vspath = r"C:\Program Files (x86)\Microsoft Visual Studio %s.0\Common7\IDE" % vsversion
 msbuild = r"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 if not os.path.exists(msbuild):
   raise Exception('no visual studio executable found')
-
-fabricThirdParty = os.path.join(os.environ['FABRIC_SCENE_GRAPH_DIR'], 'ThirdParty', 'PreBuilt', 'Windows', 'x86_64', 'VS2013', 'Release')
-zlibpath = os.path.join(fabricThirdParty, 'zlib', '1.2.8')
-boostpath = os.path.join(fabricThirdParty, 'boost', '1.55.0')
 
 #========================================= clean =====================================
 if target in ['clean']:
@@ -60,8 +56,11 @@ if target in ['clean']:
 
 #========================================= helpers =====================================
 
-def requiresBuild(name, dependencies=[]):
-  if not target in ['all', name] + dependencies:
+def requiresBuild(name, dependencies=[], excludeAll=False):
+  targets = [name] + dependencies
+  if not excludeAll:
+    targets += ['all']
+  if not target in targets:
     return False
   marker = os.path.join(build, name, '.'+name+'.marker')
   return not os.path.exists(marker)
@@ -88,6 +87,8 @@ def patchSourceFile(sourceFile, patchFile):
   cmd = ['patch', sourceFile, patchFile]
   p = subprocess.Popen(cmd, cwd=os.path.split(sourceFile)[0])
   p.wait()
+  if p.returncode != 0:
+    raise Exception('patchSourceFile failed')
 
 def runMSBuild(project, buildpath):
   env = {}
@@ -97,43 +98,33 @@ def runMSBuild(project, buildpath):
   cmd = [msbuild, project, '/t:build', '/p:Configuration=Release', '/p:Configuration=Release', '/p:Platform=x64']
   p = subprocess.Popen(cmd, cwd=buildpath, env=env)
   p.wait()
+  if p.returncode != 0:
+    raise Exception('runMSBuild failed')
 
 def runCMake(name, folder, projects, flags={}, env={}, subfolder='build'):
-  sourcepath = os.path.join(build, name, folder)
+  sourcepath = folder
+  if not os.path.isabs(sourcepath):
+    sourcepath = os.path.join(build, name, sourcepath)
   buildpath = os.path.join(build, name, subfolder)
   if not os.path.exists(buildpath):
     os.makedirs(buildpath)
 
   env.update(os.environ)
 
+  # cmd = ['cmake', "--trace", "--debug-output", "-G", "Visual Studio %s Win64" % vsversion, sourcepath]
   cmd = ['cmake', "-G", "Visual Studio %s Win64" % vsversion, sourcepath]
   for flag in flags:
     cmd += ['-D%s=%s' % (flag, flags[flag])]
   p = subprocess.Popen(cmd, cwd=buildpath, env=env)
   p.wait()
+  if p.returncode != 0:
+    raise Exception('runCMake failed')
 
   for project in projects:
     runMSBuild('%s.vcxproj' % project, buildpath)
 
   marker = os.path.join(build, name, '.'+name+'.marker')
   open(marker, 'wb').write('done')
-
-# def runScons(name, folder):
-#   sourcepath = os.path.join(build, name, folder)
-#   buildpath = os.path.join(build, name, 'build')
-#   if not os.path.exists(buildpath):
-#     os.makedirs(buildpath)
-
-#   env = {}
-#   env.update(os.environ)
-#   env['MSVC_VERSION'] = '%s.0' % vsversion
-#   cmd = ['scons', '-f', os.path.join(sourcepath, 'SConstruct')]
-#   print cmd
-#   p = subprocess.Popen(cmd, cwd=buildpath)
-#   p.wait()
-
-  # marker = os.path.join(build, name, '.'+name+'.marker')
-  # open(marker, 'wb').write('done')
 
 def stageResults(name, includeFolders, libraryFolders):
   sources = {
@@ -150,7 +141,6 @@ def stageResults(name, includeFolders, libraryFolders):
           if not f.rpartition('.')[2].lower() in filters:
             continue
           path = os.path.join(root, f)
-          print "Installing %s" % path
           if key == 'lib':
             targetpath = os.path.join(stage, 'lib', f)
           else:
@@ -159,7 +149,39 @@ def stageResults(name, includeFolders, libraryFolders):
           targetdir = os.path.split(targetpath)[0]
           if not os.path.exists(targetdir):
             os.makedirs(targetdir)
+          print "Installing %s" % targetpath
           shutil.copyfile(path, targetpath)
+
+#========================================= zlib ====================================
+
+if requiresBuild('zlib'):
+  # tbb on windows uses a drop from https://github.com/wjakob/tbb/tree/tbb43u6
+  extractSourcePackage('zlib', 'zlib-1.2.11', 'zlib-1.2.11.zip')
+  runCMake('zlib', 'zlib-1.2.11', ['zlibstatic'])
+
+  stageResults('zlib', [
+    os.path.join(build, 'zlib', 'zlib-1.2.11'),
+    os.path.join(build, 'zlib', 'build')
+    ], [
+    os.path.join(build, 'zlib', 'build', 'Release')
+    ])
+
+#========================================= boost ====================================
+
+# we'll use 163 binaries for visual studio 14.0
+# https://sourceforge.net/projects/boost/files/boost-binaries/1.63.0/
+boostdir = os.path.join(root, 'boost')
+if not os.path.exists(boostdir):
+  raise Exception('You need to install boost to %s or create a link there.' % boostdir)
+boostincludepath = boostdir
+boostlibrarypath = None
+for subfolder in glob.glob(boostdir+'/*'):
+  subfoldername = os.path.split(subfolder)[1]
+  if subfoldername.startswith('lib64') and subfoldername.endswith('%s.0' % vsversion):
+    boostlibrarypath = subfolder
+    break
+if boostlibrarypath is None:
+  raise Exception('Boost library path %s/lib64-msvc-%s.0 not found.' % (boostdir, vsversion))
 
 #========================================= tbb =====================================
 
@@ -186,10 +208,6 @@ if requiresBuild('double-conversion'):
     os.path.join(build, 'double-conversion', 'build', 'src', 'Release')
     ])
 
-#========================================== boost ====================================
-
-# we are going to use boost from the fabric software thirdparty folder
-
 #========================================= ilmbase ===================================
 
 if requiresBuild('ilmbase', ['openexr']):
@@ -205,8 +223,10 @@ if requiresBuild('ilmbase', ['openexr']):
 #========================================== hdf5 =====================================
 
 if requiresBuild('hdf5', ['alembic']):
-  extractSourcePackage('hdf5', 'hdf5-1.8.9', 'hdf5-1.8.9.tar.bz2')
-  runCMake('hdf5', 'hdf5-1.8.9', ['src/hdf5', 'hl/src/hdf5_hl'], flags={'BUILD_SHARED_LIBS': 'off', 'HDF5_BUILD_HL_LIB': 'on'})
+  if extractSourcePackage('hdf5', 'hdf5-1.8.9', 'hdf5-1.8.9.tar.bz2'):
+    patchSourceFile('hdf5/hdf5-1.8.9/config/cmake/ConfigureChecks.cmake', 'hdf5/ConfigureChecks.cmake.patch')
+
+  runCMake('hdf5', 'hdf5-1.8.9', ['src/hdf5', 'hl/src/hdf5_hl'], flags={'BUILD_SHARED_LIBS': 'off', 'HDF5_BUILD_HL_LIB': 'on', 'H5_HAVE_TIMEZONE': 'off'})
 
   stageResults('hdf5', [
     os.path.join(build, 'hdf5', 'hdf5-1.8.9', 'src'),
@@ -227,8 +247,8 @@ if requiresBuild('openexr'):
   runCMake('openexr', 'openexr-2.2.0', ['IlmImf/IlmImf', 'IlmImf/dwaLookups', 'IlmImfUtil/IlmImfUtil'], 
     flags={
       'BUILD_SHARED_LIBS': 'off', 
-      'ZLIB_INCLUDE_DIR': os.path.join(zlibpath, 'include'),
-      'ZLIB_LIBRARY': os.path.join(zlibpath, 'lib', 'zlibstatic.lib'),
+      'ZLIB_INCLUDE_DIR': os.path.join(stage, 'include', 'zlib'),
+      'ZLIB_LIBRARY': os.path.join(stage, 'lib', 'zlibstatic.lib'),
       'ILMBASE_INCLUDE_DIR': ilmbasesourcepath,
       'ILMBASE_LIBRARY_DIR': ilmbasebuildpath,
     })
@@ -246,30 +266,14 @@ if requiresBuild('ptex', ['opensubdiv']):
   runCMake('ptex', 'ptex-2.0.41/src', ['ptex/Ptex_static'],
     flags={
       'BUILD_SHARED_LIBS': 'off', 
-      'ZLIB_INCLUDE_DIR': os.path.join(zlibpath, 'include'),
-      'ZLIB_LIBRARY': os.path.join(zlibpath, 'lib', 'zlibstatic.lib'),
+      'ZLIB_INCLUDE_DIR': os.path.join(stage, 'include', 'zlib'),
+      'ZLIB_LIBRARY': os.path.join(stage, 'lib', 'zlibstatic.lib'),
     })
 
   stageResults('ptex', [
     os.path.join(build, 'ptex', 'ptex-2.0.41', 'src', 'ptex')
     ], [
     os.path.join(build, 'ptex', 'build', 'ptex', 'Release')
-    ])
-
-#========================================== glew =====================================
-
-if requiresBuild('glew', ['opensubdiv']):
-  extractSourcePackage('glew', 'glew-1.13.0', 'glew-1.13.0.tgz')
-  buildpath = os.path.join(build, 'glew', 'glew-1.13.0', 'build', 'vc%s' % vsversion)
-  runMSBuild('glew_static.vcxproj', buildpath)
-
-  marker = os.path.join(build, 'glew', '.glew.marker')
-  open(marker, 'wb').write('done')
-
-  stageResults('glew', [
-    os.path.join(build, 'glew', 'glew-1.13.0', 'include')
-    ], [
-    os.path.join(build, 'glew', 'glew-1.13.0', 'lib')
     ])
 
 #======================================== opensubdiv =================================
@@ -286,14 +290,13 @@ if requiresBuild('opensubdiv'):
   runCMake('opensubdiv', 'OpenSubdiv-3_0_5', ['opensubdiv/osd_static_cpu'],
     flags={
       'BUILD_SHARED_LIBS': 'off', 
-      # 'ZLIB_INCLUDE_DIR': os.path.join(zlibpath, 'include'),
-      # 'ZLIB_LIBRARY': os.path.join(zlibpath, 'lib', 'zlibstatic.lib'),
-      'PTEX_INCLUDE_DIR': os.path.join(zlibpath, 'include'),
+      'ZLIB_INCLUDE_DIR': os.path.join(stage, 'include', 'zlib'),
+      'ZLIB_LIBRARY': os.path.join(stage, 'lib', 'zlibstatic.lib'),
+      'PTEX_INCLUDE_DIR': os.path.join(stage, 'include'),
       'PTEX_LIBRARY': os.path.join(ptexbuildpath, 'ptex', 'Release', 'Ptex.lib'),
-      # 'GLEW_INCLUDE_DIR': os.path.join(glewsourcepath, 'include'),
-      # 'GLEW_LIBRARY': os.path.join(glewsourcepath, 'lib', 'Release', 'x64', 'glew32s.lib'),
       'TBB_INCLUDE_DIR': os.path.join(tbbsourcepath, 'include'),
       'TBB_LIBRARIES': os.path.join(tbbbuildpath, 'Release'),
+
       'NO_LIB': 'off',
       'NO_EXAMPLES': 'on',
       'NO_TUTORIALS': 'on',
@@ -337,9 +340,10 @@ if requiresBuild('alembic'):
     ],
     flags={
       'BUILD_SHARED_LIBS': 'off', 
-      'ZLIB_INCLUDE_DIR': os.path.join(zlibpath, 'include'),
-      'ZLIB_LIBRARY': os.path.join(zlibpath, 'lib', 'zlibstatic.lib'),
-      'BOOST_ROOT': boostpath,
+      'ZLIB_INCLUDE_DIR': os.path.join(stage, 'include', 'zlib'),
+      'ZLIB_LIBRARY': os.path.join(stage, 'lib', 'zlibstatic.lib'),
+      'BOOST_INCLUDEDIR': boostincludepath,
+      'BOOST_LIBRARYDIR': boostlibrarypath,
       'ALEMBIC_ILMBASE_INCLUDE_DIRECTORY': os.path.join(stage, 'include', 'ilmbase'),
       'ILMBASE_ROOT': stage,
       'ILMBASE_LIBRARY_DIR': os.path.join(stage, 'lib'),
@@ -349,6 +353,7 @@ if requiresBuild('alembic'):
       'ALEMBIC_ILMBASE_IMATH_LIB': os.path.join(stage, 'lib', 'Imath-2_2.lib'),
       'ALEMBIC_HDF5_INCLUDE_PATH': os.path.join(stage, 'include', 'hdf5'),
       'ALEMBIC_HDF5_LIBS': os.path.join(stage, 'lib', 'hdf5.lib'),
+
       'USE_PYILMBASE': 'off',
       'USE_PRMAN': 'off',
       'USE_ARNOLD': 'off',
@@ -361,6 +366,39 @@ if requiresBuild('alembic'):
     ], [
     os.path.join(build, 'alembic', 'build', 'lib')
     ])
+
+#======================================== opensubdiv =================================
+
+if requiresBuild('usd', excludeAll=True):
+  sourcepath = os.path.join(root, 'USD')
+  if not os.path.exists(sourcepath):
+    raise Exception('Need to clone USD to %s' % sourcepath)
+
+  tbbsourcepath = os.path.join(build, 'tbb', 'tbb-tbb43u6')
+  tbbbuildpath = os.path.join(build, 'tbb', 'build')
+
+  runCMake('USD', sourcepath, ['ALL_BUILD'],
+    flags = {
+      'BOOST_INCLUDEDIR': boostincludepath,
+      'BOOST_LIBRARYDIR': boostlibrarypath,
+      'TBB_INCLUDE_DIR': os.path.join(tbbsourcepath, 'include'),
+      'TBB_LIBRARIES': os.path.join(tbbbuildpath, 'Release'),
+      'OPENEXR_INCLUDE_DIR': os.path.join(stage, 'include'),
+      'OPENEXR_LIBRARY_DIR': os.path.join(stage, 'lib'),
+
+      'PXR_STRICT_BUILD_MODE': 'off',
+      'PXR_VALIDATE_GENERATED_CODE': 'off',
+      'PXR_BUILD_TESTS': 'off',
+      'PXR_BUILD_IMAGING': 'off',
+      'PXR_BUILD_USD_IMAGING': 'off',
+      'PXR_BUILD_KATANA_PLUGIN': 'off',
+      'PXR_BUILD_MAYA_PLUGIN': 'off',
+      'PXR_BUILD_ALEMBIC_PLUGIN': 'off',
+      'PXR_ENABLE_MULTIVERSE_SUPPORT': 'off',
+      'PXR_MAYA_TBB_BUG_WORKAROUND': 'off',
+      'PXR_ENABLE_NAMESPACES': 'off',
+    })
+
 
 # not needed:
 # oiio-Release-1.5.20.tar.gz ?
