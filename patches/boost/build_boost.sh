856f74ab9ac67614449b377aeb5be4bb05919050
Helge Mathee <helge.mathee@fabricengine.com>
2017-04-02 19:01:08 +0200

    centos 6 support continues

--- patches/boost/build_boost.sh
+++ patches/boost/build_boost.sh
@@ -5,4 +5,5 @@ chmod +x bootstrap.sh
 ./bootstrap.sh
 
 # Most libraries can be static libs
-./b2 -j8 --toolset-gcc-4.8 cflags=-fPIC address-model=64 architecture=x86 variant=release link=static threading=multi runtime-link=static --with-date_time --with-iostreams --with-program_options --with-python --with-regex --with-system --stagedir={{BUILDPATH}} stage
+# ./b2 -j8 --toolset-gcc-4.8 cflags=-fPIC address-model=64 architecture=x86 variant=release link=static threading=multi runtime-link=static --with-date_time --with-iostreams --with-program_options --with-python --with-regex --with-system --stagedir={{BUILDPATH}} stage
+./b2 -j8 cflags=-fPIC address-model=64 architecture=x86 variant=release link=static threading=multi runtime-link=static --with-date_time --with-iostreams --with-program_options --with-python --with-regex --with-system --stagedir={{BUILDPATH}} stage
