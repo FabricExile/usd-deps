abae6eefda1227223cf445ef09895cdf1f7c7de4
Helge Mathee <helge.mathee@fabricengine.com>
2017-04-03 13:55:44 +0200

    darwin boost build

--- patches/boost/build_boost_darwin.sh
+++ patches/boost/build_boost_darwin.sh
@@ -5,4 +5,4 @@ chmod +x bootstrap.sh
 ./bootstrap.sh
 
 # Most libraries can be static libs
-./b2 -j8 toolset=clang cflags=-fPIC cxxflags="-stdlib=libc++" linkflags="-stdlib=libc++" address-model=64 architecture=x86 variant=release link=static threading=multi runtime-link=static --with-date_time --with-iostreams --with-program_options --with-python --with-regex --with-system --stagedir={{BUILDPATH}} stage
+./b2 -j8 toolset=clang cflags=-fPIC cxxflags="-stdlib=libc++" linkflags="-stdlib=libc++" architecture=x86_64 variant=release link=static threading=multi runtime-link=static --with-date_time --with-iostreams --with-program_options --with-python --with-regex --with-system --stagedir={{BUILDPATH}} stage
