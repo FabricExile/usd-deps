f6437fed3dcac3cff4dfaf7ac7a7e154ec36c29d
Helge Mathee <helge.mathee@fabricengine.com>
2017-04-03 13:34:22 +0200

    patch file utils

--- build-fabric-usd-deps.py
+++ build-fabric-usd-deps.py
@@ -175,7 +175,8 @@ def runCMake(name, folder, projects, flags={}, env={}, subfolder='build', config
   if platform.system() == 'Windows':
     cmd = ['cmake', "-G", "Visual Studio %s" % vsversion, sourcepath]
   elif platform.system() == 'Darwin':
-    cmd = ['cmake', "-G", "Xcode", sourcepath]
+    # cmd = ['cmake', "-G", "Xcode", sourcepath]
+    cmd = ['cmake', sourcepath]
   else:
     cmd = ['cmake', sourcepath]
   for flag in flags:
